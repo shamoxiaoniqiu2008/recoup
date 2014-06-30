b91f476e503c2e533845760ba12f3113a0e6aa43
justin <justin@justin-PC>
2014-06-30 17:04:31 +0800

    报销功能1

--- JavaSource/controller/recoup/RecoupApplyController.java
+++ JavaSource/controller/recoup/RecoupApplyController.java
@@ -4,6 +4,16 @@
 package controller.recoup;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import service.recoup.RecoupApplyService;
+
+import domain.recoup.RecoupApplyDetailExtend;
+import domain.recoup.RecoupApplyRecordExtend;
 
 /**
  * @author justin
@@ -11,14 +21,125 @@ import java.io.Serializable;
  */
 
 public class RecoupApplyController implements Serializable{
+	
+	
+	  
+		/** 
+		* @Fields serialVersionUID : TODO
+		* @version V1.0
+		*/   
+	    
+	private static final long serialVersionUID = 100000000000000L;
+	
+	private transient RecoupApplyService recoupApplyService;
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+	private RecoupApplyRecordExtend recordForAdd = new RecoupApplyRecordExtend();
+
+	private List<RecoupApplyDetailExtend> detailListForAdd = new ArrayList<RecoupApplyDetailExtend>();
+	
+	private RecoupApplyDetailExtend detailForAdd = new RecoupApplyDetailExtend();
 	
 	
 	
-	
+	public void addDetailList(){
+		RecoupApplyDetailExtend detail = new RecoupApplyDetailExtend();
+		detail = detailForAdd;
+		detailListForAdd.add(detail);
+		FacesContext.getCurrentInstance().addMessage(
+				null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO,
+						"明细添加成功！" , "明细添加成功！"));
+	}
+
+	  
+		/**
+		 * @return the recordForAdd
+		 */  
+	    
+	public RecoupApplyRecordExtend getRecordForAdd() {
+		return recordForAdd;
+	}
+
+
+
+	  
+	    /**
+		 * @param recordForAdd the recordForAdd to set
+		 */  
+	    
+	public void setRecordForAdd(RecoupApplyRecordExtend recordForAdd) {
+		this.recordForAdd = recordForAdd;
+	}
+
+
+
+
+		  
+			/**
+			 * @return the detailListForAdd
+			 */  
+		    
+		public List<RecoupApplyDetailExtend> getDetailListForAdd() {
+			return detailListForAdd;
+		}
+
+
+
+
+		  
+		    /**
+			 * @param detailListForAdd the detailListForAdd to set
+			 */  
+		    
+		public void setDetailListForAdd(List<RecoupApplyDetailExtend> detailListForAdd) {
+			this.detailListForAdd = detailListForAdd;
+		}
+
+
+
+
+			  
+				/**
+				 * @return the detailForAdd
+				 */  
+			    
+			public RecoupApplyDetailExtend getDetailForAdd() {
+				return detailForAdd;
+			}
+
+
+
+
+			  
+			    /**
+				 * @param detailForAdd the detailForAdd to set
+				 */  
+			    
+			public void setDetailForAdd(RecoupApplyDetailExtend detailForAdd) {
+				this.detailForAdd = detailForAdd;
+			}
+
+
+				  
+					/**
+					 * @return the recoupApplyService
+					 */  
+				    
+				public RecoupApplyService getRecoupApplyService() {
+					return recoupApplyService;
+				}
+
+
+				  
+				    /**
+					 * @param recoupApplyService the recoupApplyService to set
+					 */  
+				    
+				public void setRecoupApplyService(RecoupApplyService recoupApplyService) {
+					this.recoupApplyService = recoupApplyService;
+				}
+
+
+
 
 }
