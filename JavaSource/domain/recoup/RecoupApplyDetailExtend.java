8ecab81a4d6df03074916d6cafd4049535633b5a
justin <justin@justin-PC>
2014-06-30 20:19:39 +0800

    申请主记录和申请明细记录domain扩展类添加

---
+++ JavaSource/domain/recoup/RecoupApplyDetailExtend.java
@@ -0,0 +1,34 @@
+  
+	/**  
+	* @Title: RecoupApplyDetailExtend.java 
+	* @Package domain 
+	* @Description: TODO
+	* @author Justin.Su
+	* @date 2014-6-30 上午11:08:18 
+	* @version V1.0
+	* @Copyright: Copyright (c) Centling Co.Ltd. 2014
+	* ★★★★★★★★版权所有※拷贝必究 ★★★★★★★★
+	*/ 
+    
+package domain.recoup;
+
+  
+	/** 
+ * @ClassName: RecoupApplyDetailExtend 
+ * @Description: TODO
+ * @author Justin.Su
+ * @date 2014-6-30 上午11:08:18
+ * @version V1.0 
+ */
+
+public class RecoupApplyDetailExtend extends RecoupApplyDetail{
+			/** 
+			* @Fields serialVersionUID : TODO
+			* @version V1.0
+			*/   
+		    
+		private static final long serialVersionUID = 10L;
+		 
+		
+
+}
---
+++ JavaSource/domain/recoup/RecoupApplyRecordExtend.java
@@ -0,0 +1,35 @@
+  
+	/**  
+	* @Title: RecoupApplyRecordExtend.java 
+	* @Package domain.recoup 
+	* @Description: TODO
+	* @author Justin.Su
+	* @date 2014-6-30 上午11:07:26 
+	* @version V1.0
+	* @Copyright: Copyright (c) Centling Co.Ltd. 2014
+	* ★★★★★★★★版权所有※拷贝必究 ★★★★★★★★
+	*/ 
+    
+package domain.recoup;
+
+  
+	/** 
+ * @ClassName: RecoupApplyRecordExtend 
+ * @Description: TODO
+ * @author Justin.Su
+ * @date 2014-6-30 上午11:07:26
+ * @version V1.0 
+ */
+
+public class RecoupApplyRecordExtend extends  RecoupApplyRecord{
+
+		  
+			/** 
+			* @Fields serialVersionUID : TODO
+			* @version V1.0
+			*/   
+	
+		    
+		private static final long serialVersionUID = 1000000001L;
+
+}
